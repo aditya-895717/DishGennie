2f925d1ca50dcd5567c734fcd24d5c7d6c0e13ac
Aditya Dubey <166470390+aditya-895717@users.noreply.github.com>
2026-01-07 12:49:45 +0530

    Refactor email backend settings in settings.py
    Updated email configuration settings for Django.

--- Dishgennie/settings.py
+++ Dishgennie/settings.py
@@ -28,13 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["dishgennie.onrender.com"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'adityadubey1431@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'qslb fopw jueu gzty')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-DEFAULT_FROM_EMAIL = 'DishGennie <adityadubey1431@gmail.com>'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'adityadubey1431@gmail.com'
+EMAIL_HOST_PASSWORD = 'qslb fopw jueu gzty'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
